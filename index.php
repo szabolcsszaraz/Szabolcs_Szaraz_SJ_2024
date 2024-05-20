ed58fd89627385b68412eaad367029f9703be180
szabolcsszarazDDDDDD <szarazszabolcs22@gmail.com>
2024-05-20 13:00:24 +0200

    slider na php

--- index.php
+++ index.php
@@ -10,48 +10,48 @@
 
         <div class="carousel-inner" role="listbox">
 
-          <!-- Slide 1 -->
-          
-          <div class="carousel-item active" style="background-image: url(assets/img/slide/slide-1.jpg);">
-            <div class="carousel-container">
-              <div class="carousel-content">
-                <h2 class="animate__animated animate__fadeInDown"><span>Delicious</span> Restaurant</h2>
-                <p class="animate__animated animate__fadeInUp">Ut velit est quam dolor ad a aliquid qui aliquid. Sequi ea ut et est quaerat sequi nihil ut aliquam. Occaecati alias dolorem mollitia ut. Similique ea voluptatem. Esse doloremque accusamus repellendus deleniti vel. Minus et tempore modi architecto.</p>
-                <div>
-                  <a href="menu.php" class="btn-menu animate__animated animate__fadeInUp scrollto">Our Menu</a>
-                  <a href="#book-a-table" class="btn-book animate__animated animate__fadeInUp scrollto">Book a Table</a>
-                </div>
-              </div>
-            </div>
-          </div>
+        <!--We define an array $slides containing data for each slide. Each slide is an associative array with keys for active, image, title, and description.
+        We use a foreach loop to iterate over the $slides array and generate the HTML for each slide.
+        The <php if ($slide['active']) echo 'active'; ?> part ensures that the active class is added to the first slide or any other slide marked as active in the array.
+        The slide content is dynamically inserted into the HTML using PHP's echo.-->
+        <?php
+          $slides = [
+              [
+                  'active' => true,
+                  'image' => 'assets/img/slide/slide-1.jpg',
+                  'title' => '<span>Delicious</span> Restaurant',
+                  'description' => 'Ut velit est quam dolor ad a aliquid qui aliquid. Sequi ea ut et est quaerat sequi nihil ut aliquam. Occaecati alias dolorem mollitia ut. Similique ea voluptatem. Esse doloremque accusamus repellendus deleniti vel. Minus et tempore modi architecto.',
+              ],
+              [
+                  'active' => false,
+                  'image' => 'assets/img/slide/slide-2.jpg',
+                  'title' => 'Lorem Ipsum Dolor',
+                  'description' => 'Ut velit est quam dolor ad a aliquid qui aliquid. Sequi ea ut et est quaerat sequi nihil ut aliquam. Occaecati alias dolorem mollitia ut. Similique ea voluptatem. Esse doloremque accusamus repellendus deleniti vel. Minus et tempore modi architecto.',
+              ],
+              [
+                  'active' => false,
+                  'image' => 'assets/img/slide/slide-3.jpg',
+                  'title' => 'Sequi ea ut et est quaerat',
+                  'description' => 'Ut velit est quam dolor ad a aliquid qui aliquid. Sequi ea ut et est quaerat sequi nihil ut aliquam. Occaecati alias dolorem mollitia ut. Similique ea voluptatem. Esse doloremque accusamus repellendus deleniti vel. Minus et tempore modi architecto.',
+              ],
+          ];
+        ?>
 
-          <!-- Slide 2 -->
-          <div class="carousel-item" style="background-image: url(assets/img/slide/slide-2.jpg);">
-            <div class="carousel-container">
-              <div class="carousel-content">
-                <h2 class="animate__animated animate__fadeInDown">Lorem Ipsum Dolor</h2>
-                <p class="animate__animated animate__fadeInUp">Ut velit est quam dolor ad a aliquid qui aliquid. Sequi ea ut et est quaerat sequi nihil ut aliquam. Occaecati alias dolorem mollitia ut. Similique ea voluptatem. Esse doloremque accusamus repellendus deleniti vel. Minus et tempore modi architecto.</p>
-                <div>
+  <?php foreach ($slides as $index => $slide): ?>
+  <div class="carousel-item <?php if ($slide['active']) echo 'active'; ?>" style="background-image: url(<?php echo $slide['image']; ?>);">
+      <div class="carousel-container">
+          <div class="carousel-content">
+              <h2 class="animate__animated animate__fadeInDown"><?php echo $slide['title']; ?></h2>
+              <p class="animate__animated animate__fadeInUp"><?php echo $slide['description']; ?></p>
+              <div>
                   <a href="menu.php" class="btn-menu animate__animated animate__fadeInUp scrollto">Our Menu</a>
                   <a href="#book-a-table" class="btn-book animate__animated animate__fadeInUp scrollto">Book a Table</a>
-                </div>
               </div>
-            </div>
           </div>
+      </div>
+  </div>
+<?php endforeach; ?>
 
-          <!-- Slide 3 -->
-          <div class="carousel-item" style="background-image: url(assets/img/slide/slide-3.jpg);">
-            <div class="carousel-container">
-              <div class="carousel-content">
-                <h2 class="animate__animated animate__fadeInDown">Sequi ea ut et est quaerat</h2>
-                <p class="animate__animated animate__fadeInUp">Ut velit est quam dolor ad a aliquid qui aliquid. Sequi ea ut et est quaerat sequi nihil ut aliquam. Occaecati alias dolorem mollitia ut. Similique ea voluptatem. Esse doloremque accusamus repellendus deleniti vel. Minus et tempore modi architecto.</p>
-                <div>
-                  <a href="menu.php" class="btn-menu animate__animated animate__fadeInUp scrollto">Our Menu</a>
-                  <a href="#book-a-table" class="btn-book animate__animated animate__fadeInUp scrollto">Book a Table</a>
-                </div>
-              </div>
-            </div>
-          </div>
 
         </div>
 
