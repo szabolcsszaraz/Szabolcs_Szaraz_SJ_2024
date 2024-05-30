27eac36703f9f0fb255b7c649955e6e8cfada949
szabolcsszarazDDDDDD <szarazszabolcs22@gmail.com>
2024-05-30 17:27:13 +0200

    Page, Database

---
+++ _inc/classes/Database.php
@@ -0,0 +1,37 @@
+<?php 
+
+    class Database{
+
+        private $host = 'localhost';
+        private $db_name = 'szabi_test';
+        private $user_name = 'root';
+        private $password = '';
+
+        protected $connection;
+
+        public function __construct(){
+            $this->connection = $this->db_connection();
+        }
+
+        public function __destruct() {
+            // Uzatvorenie spojenia 
+            $this->connection = null;
+        }
+
+        protected function db_connection(){
+            try {
+                
+                $this->connection = new PDO("mysql:host=" . $this->host . ";dbname=" . $this->db_name . ";charset=utf8", 
+                                    $this->user_name, 
+                                    $this->password
+                                    );
+                $this->connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
+                return $this->connection;
+            }catch(PDOException $e){
+                return 0;
+                die("Chyba pripojenia k databáze: " . $e->getMessage());
+            }
+        }
+    }
+
+?>
---
+++ _inc/classes/Page.php
@@ -0,0 +1,15 @@
+<?php
+
+    class Page{
+        private $page_name;
+
+        public function __construct($page_name)
+        {
+            $this->page_name = $page_name;
+        }
+
+        
+
+    }
+
+?>
